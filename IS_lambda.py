f52319957cd5c2fc2b6a87b43c9c3c057cf52655
Marc Brophy <aldrichbrophy@gmail.com>
2023-05-24 14:37:13 -0700

    added delete all method.

--- IS_lambda.py
+++ IS_lambda.py
@@ -12,6 +12,7 @@ table = dynamodb.Table(dynamoName)
 
 getMethod = "GET"
 postMethod = "POST"
+deleteMethod = "DELETE"
 
 healthPath = "/health"
 productPath = "/product"
@@ -29,12 +30,40 @@ def lambda_handler(event, context):
         response = saveProduct(json.loads(event['body']))
     elif httpMethod == getMethod and path == productsPath:
         response = getProducts()
+    elif httpMethod == getMethod and path == productPath:
+        response = getProduct(event['queryStringParameters']['productId'])
+    elif httpMethod == deleteMethod and path == productsPath:
+        response = deleteProducts()
     else:
         response = buildResponse(404, 'Not Found')
     
     return response
 
 
+def deleteProducts():
+    
+    try:
+        response = table.scan()
+        result = response['Items']
+
+        # Delete each item individually
+        for item in result:
+            table.delete_item(
+                Key = {
+                    'productId': item['productId']
+                },
+                ReturnValues = 'ALL_OLD'
+            )
+
+        body = {
+            'Operation': 'DELETE',
+            'Message': 'SUCCESS',
+            'deletedItems': result
+        }
+        return buildResponse(200, body)
+    
+    except:
+        logger.exception("Error deleting all items.")
 # Gets all items from the product table in DynamoDB
 def getProducts():
     try:
@@ -51,7 +80,21 @@ def getProducts():
 
         return buildResponse(200, body)
     except:
-        logger.exception("Exception logged.")
+        logger.exception("Error getting all products.")
+
+def getProduct(productId):
+    try:
+        response = table.get_item(
+            Key = {
+                'productId': productId
+            }
+        )
+        if 'Item' in response:
+            return buildResponse(200, response['Item'])
+        else:
+            return buildResponse(404, {'Message': 'id: %s not found' % productId})
+    except:
+        logger.exception("Error obtaining product " + str(productId) + ".")
 
 # posts a product into dynamoDB
 def saveProduct(requestBody):
@@ -64,7 +107,7 @@ def saveProduct(requestBody):
         }
         return buildResponse(200, body)
     except:
-        logger.exception("Exception logged.")
+        logger.exception("Error saving product.")
 
 # Response builder.
 def buildResponse(statusCode, body = None):
